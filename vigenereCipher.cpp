2574b86a51a5728e850acba685434fb84e5398fb
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-11 15:53:50 +0300

    simple vigenereCipher implementation

---
+++ vigenereCipher.cpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include "caesarCipher.h"
+
+VigenereCipher::VigenereCipher(const std::string& key) {
+	this->key = key;
+}
+
+std::string VigenereCipher::encrypt(const std::string& text) {
+	int index = 0;
+	for (int i = 0; text[i] != '\0'; i++) {
+		int shift = key[index] - 'a';
+		text[i] = text[i] + shift;
+		if (index == key.size() - 1) {
+			index = 0;
+		} else {
+			index++;
+		}
+	}
+	return text;
+}
+
+std::string VigenereCipher::decrypt(const std::string& text) {
+	int index = 0;
+	for (int i = 0; text[i] != '\0'; i++) {
+		int shift = key[index] - 'a';
+		text[i] = text[i] - shift;
+		if (index == key.size() - 1) {
+			index = 0;
+		} else {
+			index++;
+		}
+	}
+	return text;
+}
