b0abe9dae3e5a24d0d1e03b0a72ee23c45521e0b
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-11 15:25:13 +0300

    added caesarCipher derived class

---
+++ caesarCipher.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+class CaesarCipher : public Cipher {
+	int key_;
+	public:
+	CaesarCipher(int key);
+	std::string encrypt(const std::string& text) override;
+	std::string decrypt(const std::string& text) override;
+};
