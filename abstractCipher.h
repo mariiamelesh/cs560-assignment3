abdd5844edbc26bab1c3357d852fee15a6beb508
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-11 15:21:21 +0300

    cipher abstract class file

---
+++ abstractCipher.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+class Cipher {
+public:
+	virtual std::string encrypt(const std::string& text) = 0;
+	virtual std::string decrypt(const std::string& text) = 0;
+	virtual ~Cipher() = default;
+};
