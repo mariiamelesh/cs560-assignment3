7e96cf83660091e52ce94a4517cf57654d68558c
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-11 15:41:17 +0300

    +implemented decrypt function

--- caesarCipher.cpp
+++ caesarCipher.cpp
@@ -10,4 +10,12 @@ std::string CaesarCipher::encrypt(const std::string& text) {
 	for (int i = 0; text[i] != '\0'; i++) {
 		text[i] = text[i] + key;
 	}
+	return text;
+}
+
+std::string CaesarCipher::decrypt(const std::string& text) {
+	for (int i = 0; text[i] != '\0'; i++) {
+		text[i] = text[i] - key;
+	}
+	return text;
 }
