5a09f305d47527b7b4bfa51bc7ad01c3dff9e80c
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-12 06:41:58 +0300

    started making console menu

--- main.cpp
+++ main.cpp
@@ -9,6 +9,27 @@ using Decrypt = char*(*)(void*, const char*);
 using Destroy = void(*)(void*);
 using Free = void(*)(char*);
 
+void console(HINSTANCE lib) {
+	int running = 1;
+	while (running) {
+		int choice;
+		std::cout << "choose cipher: 1 - caesar 2 - vigenere\n> ";
+		std::cin >> choice;
+		switch {
+			case 1:
+				int key;
+				std::string text;
+				std::cout << "write your key\n> ";
+				std::cin >> key;
+				void* caesar = createCaesar(key);
+				std::cout << "write your text\n> ";
+				std::cin >> text;
+				char* encrypted = cipherEncrypt(caesar, text.c_str());
+				break;
+		}
+	}
+}
+
 int main() {
 	HINSTANCE lib = LoadLibrary(TEXT("cipher.dll"));
     if (!lib) {
@@ -21,6 +42,8 @@ int main() {
     Decrypt cipherDecrypt = (Decrypt)GetProcAddress(lib, "cipherDecrypt");
     Destroy cipherDestroy = (Destroy)GetProcAddress(lib, "cipherDestroy");
     Free cipherFree = (Free)GetProcAddress(lib, "cipherFree");
-	//std::cout << "Hello world";
+	
+	console(lib);
+
 	return 0;
 }
