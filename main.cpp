69f39dbb983efb9638826f742e4fbc401cdd0a14
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-12 20:14:44 +0300

    raw console menu implementation

--- main.cpp
+++ main.cpp
@@ -9,26 +9,6 @@ using Decrypt = char*(*)(void*, const char*);
 using Destroy = void(*)(void*);
 using Free = void(*)(char*);
 
-void console(HINSTANCE lib) {
-	int running = 1;
-	while (running) {
-		int choice;
-		std::cout << "choose cipher: 1 - caesar 2 - vigenere\n> ";
-		std::cin >> choice;
-		switch {
-			case 1:
-				int key;
-				std::string text;
-				std::cout << "write your key\n> ";
-				std::cin >> key;
-				void* caesar = createCaesar(key);
-				std::cout << "write your text\n> ";
-				std::cin >> text;
-				char* encrypted = cipherEncrypt(caesar, text.c_str());
-				break;
-		}
-	}
-}
 
 int main() {
 	HINSTANCE lib = LoadLibrary(TEXT("cipher.dll"));
@@ -43,7 +23,65 @@ int main() {
     Destroy cipherDestroy = (Destroy)GetProcAddress(lib, "cipherDestroy");
     Free cipherFree = (Free)GetProcAddress(lib, "cipherFree");
 	
-	console(lib);
-
+	int running = 1;
+	while (running) {
+		int choice;
+		std::cout << "choose cipher: 1 - caesar 2 - vigenere\n0 - exit\n> ";
+		std::cin >> choice;
+		void* cipher = nullptr;
+		switch (choice) {
+			case 0: {
+				running = 0;
+				continue;
+			}
+			case 1: {
+				int key;
+				std::cout << "write your key\n> ";
+				std::cin >> key;
+				void* caesar = createCaesar(key);
+				cipher = caesar;
+				break;
+			}
+			case 2: {
+				std::string key;
+				std::cout << "write your key\n> ";
+				std::cin >> key;
+				void* vigenere = createVigenere(key.c_str());
+				cipher = vigenere;
+				break;
+			}
+			default:
+				std::cout << "invalid input";
+				break;
+		}
+		if (running == 0) {
+			break;
+		}
+		std::string text;
+		std::cout << "write your text\n> ";
+		std::cin.ignore();
+        std::getline(std::cin, text);
+		int choice1;
+		std::cout << "1 - encrypt, 2 - decrypt\n> ";
+		std::cin >> choice1;
+		char* result = nullptr;
+		switch (choice1) {
+			case 1:
+				result = cipherEncrypt(cipher, text.c_str());
+				std::cout << "encrypted text: " << result << std::endl;
+				break;
+			case 2:
+				result = cipherDecrypt(cipher, text.c_str());
+				std::cout << "decrypted text: " << result << std::endl;
+				break;
+		}
+		if (cipher!=nullptr) {
+			cipherDestroy(cipher);
+		}
+		if (result!=nullptr) {
+			cipherFree(result);
+		}
+	}
+	FreeLibrary(lib);
 	return 0;
 }
