423e5bc849b81c7e9094959df47a25266034317b
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-12 06:29:46 +0300

    function masks implemented

--- main.cpp
+++ main.cpp
@@ -4,11 +4,19 @@
 
 using CreateCaesar = void*(*)(int);
 using CreateVigenere = void*(*)(const char*);
-using EncryptDecrypt = char*(*)(void*, const char*);
+using Encrypt = char*(*)(void*, const char*);
+using Decrypt = char*(*)(void*, const char*);
 using Destroy = void(*)(void*);
 using Free = void(*)(char*);
 
 int main() {
+	
+	CreateCaesar createCaesar = (CreateCaesar)GetProcAddress(hLib, "cipherCreateCaesar");
+    CreateVigenere createVigenere = (CreateVigenere)GetProcAddress(hLib, "cipherCreateVigenere");
+    Encrypt cipherEncrypt = (Encrypt)GetProcAddress(hLib, "cipherEncrypt");
+    Decrypt cipherDecrypt = (Decrypt)GetProcAddress(hLib, "cipherDecrypt");
+    Destroy cipherDestroy = (Destroy)GetProcAddress(hLib, "cipherDestroy");
+    Free cipherFree = (Free)GetProcAddress(hLib, "cipherFree");
 	//std::cout << "Hello world";
 	return 0;
 }
