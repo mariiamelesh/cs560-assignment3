366ec42076477f3ddfaf6049e39ae6db5ca63f58
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-12 06:31:46 +0300

    added library loading in runtime

--- main.cpp
+++ main.cpp
@@ -10,13 +10,17 @@ using Destroy = void(*)(void*);
 using Free = void(*)(char*);
 
 int main() {
+	HINSTANCE lib = LoadLibrary(TEXT("cipher.dll"));
+    if (!lib) {
+        return 1;
+    }
 	
-	CreateCaesar createCaesar = (CreateCaesar)GetProcAddress(hLib, "cipherCreateCaesar");
-    CreateVigenere createVigenere = (CreateVigenere)GetProcAddress(hLib, "cipherCreateVigenere");
-    Encrypt cipherEncrypt = (Encrypt)GetProcAddress(hLib, "cipherEncrypt");
-    Decrypt cipherDecrypt = (Decrypt)GetProcAddress(hLib, "cipherDecrypt");
-    Destroy cipherDestroy = (Destroy)GetProcAddress(hLib, "cipherDestroy");
-    Free cipherFree = (Free)GetProcAddress(hLib, "cipherFree");
+	CreateCaesar createCaesar = (CreateCaesar)GetProcAddress(lib, "cipherCreateCaesar");
+    CreateVigenere createVigenere = (CreateVigenere)GetProcAddress(lib, "cipherCreateVigenere");
+    Encrypt cipherEncrypt = (Encrypt)GetProcAddress(lib, "cipherEncrypt");
+    Decrypt cipherDecrypt = (Decrypt)GetProcAddress(lib, "cipherDecrypt");
+    Destroy cipherDestroy = (Destroy)GetProcAddress(lib, "cipherDestroy");
+    Free cipherFree = (Free)GetProcAddress(lib, "cipherFree");
 	//std::cout << "Hello world";
 	return 0;
 }
