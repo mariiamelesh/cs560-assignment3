d8567988b8a55b4fe9c2057b7cb9b67ad41594d4
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-12 06:25:07 +0300

    created function masks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
+#include <windows.h>	
+
+using CreateCaesar = void*(*)(int);
+using CreateVigenere = void*(*)(const char*);
+using EncryptDecrypt = char*(*)(void*, const char*);
+using Destroy = void(*)(void*);
+using Free = void(*)(char*);
 
 int main() {
-	std::cout << "Hello world";
+	//std::cout << "Hello world";
 	return 0;
 }
